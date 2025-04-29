88a689a026e0ea03ef0034e916ac03c5933ad9d7
David Millman <david.millman@gmail.com>
2025-04-28 19:04:04 -0700

    Move SuperMarioEnv visualization to separate file

--- ppo_nes.py
+++ ppo_nes.py
@@ -8,7 +8,6 @@ import random
 import time
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
 
 import gymnasium as gym
 import numpy as np
@@ -16,10 +15,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import tyro
-from PIL import Image
 from torch.distributions.categorical import Categorical
 from torch.utils.tensorboard import SummaryWriter
-
 from stable_baselines3.common.atari_wrappers import (  # isort:skip
     ClipRewardEnv,
     EpisodicLifeEnv,
@@ -28,18 +25,20 @@ from stable_baselines3.common.atari_wrappers import (  # isort:skip
     NoopResetEnv,
 )
 
+
 from super_mario_env import SuperMarioEnv
+from super_mario_env_viz import render_mario_pos_value_sweep
 
 from gymnasium.envs.registration import register
 
 register(
-     id="SuperMarioBros-v0",
-     entry_point=SuperMarioEnv,
-     max_episode_steps=60 * 60 * 5,
+    id="SuperMarioBros-mame-v0",
+    entry_point=SuperMarioEnv,
+    max_episode_steps=60 * 60 * 5,
 )
 
-NdArrayUint8 = np.ndarray[np.dtype[np.uint8]]
 
+NdArrayUint8 = np.ndarray[np.dtype[np.uint8]]
 
 @dataclass
 class Args:
@@ -92,7 +91,7 @@ class Args:
     """create a checkpoint every N updates"""
 
     # Algorithm specific arguments
-    env_id: str = "SuperMarioBros-v0"
+    env_id: str = "SuperMarioBros-mame-v0"
     """the id of the environment"""
     total_timesteps: int = 10000000
     """total timesteps of the experiments"""
@@ -141,6 +140,7 @@ def make_env(env_id, idx, capture_video, run_name):
         if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array")
             env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+            raise RuntimeError("STOP")
         else:
             env = gym.make(env_id, render_mode="human")
 
@@ -155,6 +155,7 @@ def make_env(env_id, idx, capture_video, run_name):
         env = ClipRewardEnv(env)
         env = gym.wrappers.GrayscaleObservation(env)
         env = gym.wrappers.ResizeObservation(env, (84, 84))
+        # env = gym.wrappers.ResizeObservation(env, (224, 224))
         env = gym.wrappers.FrameStackObservation(env, 4)
 
         return env
@@ -180,6 +181,7 @@ class Agent(nn.Module):
             nn.ReLU(),
             nn.Flatten(),
             layer_init(nn.Linear(64 * 7 * 7, 512)),
+            # layer_init(nn.Linear(64 * 24 * 24, 512)),
             nn.ReLU(),
         )
         self.actor = layer_init(nn.Linear(512, envs.single_action_space.n), std=0.01)
@@ -195,172 +197,6 @@ class Agent(nn.Module):
         if action is None:
             action = probs.sample()
         return action, probs.log_prob(action), probs.entropy(), self.critic(hidden)
-
-
-def _set_mario_pos_in_ram(ram: NdArrayUint8, x: int, y: int, xvel: int, yvel: int):
-
-    # Current position.
-    # E.g.:
-    #   level_x=  1 pos_x=118 screen_offset= 25
-    #   level_x=  4 pos_x= 45 screen_offset=106
-    #   level_x=  3 pos_x=195 screen_offset=  0
-
-    # Player horizontal position in level.  Increments for each new full screen.
-    level_x = ram[0x006D]
-
-    # Player X position.
-    pos_x = ram[0x0086]
-
-    # Player x pos within current screen offset.
-    screen_offset_x = ram[0x03AD]
-
-    # When we set "x=0", we want the player position to be at the left edge of the screen.
-    # But, we may be transitioning between screen chunks.
-    #
-    # Example going back and forth between boundary of screen chunk:
-    #   Iter: 2726   Time left: 379   level_x=  1 screen_x=  9 screen_offset=112
-    #   Iter: 31238  Time left: 358   level_x=  1 screen_x= 33 screen_offset= 84
-    #   Iter: 41750  Time left: 337   level_x=  0 screen_x=205 screen_offset=  0
-    #   Iter: 52262  Time left: 315   level_x=  0 screen_x=205 screen_offset=  0
-    #   Iter: 62774  Time left: 294   level_x=  0 screen_x=205 screen_offset=  0
-    #   Iter: 73286  Time left: 273   level_x=  1 screen_x=  5 screen_offset= 56
-    #   Iter: 83798  Time left: 251   level_x=  1 screen_x=  5 screen_offset= 56
-    #   Iter: 94310  Time left: 230   level_x=  1 screen_x= 35 screen_offset= 80
-    #   Iter: 10822  Time left: 209   level_x=  1 screen_x=182 screen_offset=111
-    #   Iter: 11334  Time left: 187   level_x=  1 screen_x=249 screen_offset=112
-    #   Iter: 12846  Time left: 166   level_x=  2 screen_x=  2 screen_offset=112
-    #   Iter: 13358  Time left: 145   level_x=  1 screen_x=227 screen_offset= 66
-    #   Iter: 14870  Time left: 123   level_x=  1 screen_x=247 screen_offset= 80
-    #   Iter: 15382  Time left: 102   level_x=  2 screen_x= 11 screen_offset= 79
-    #   Iter: 16894  Time left: 81    level_x=  1 screen_x=249 screen_offset= 61
-    #   Iter: 17406  Time left: 59    level_x=  2 screen_x= 41 screen_offset= 83
-    #
-    # When the offset is greater than the screen position, we have to roll back the level.
-
-    # Convert into an absolute left position (instead of relative to screen chunk).
-    left_pos = int(level_x) * 256 + int(pos_x)
-
-    # The left side of the screen is going to be when screen_offset_x is 0.
-    new_left_pos = left_pos - int(screen_offset_x) + x
-
-    # Recompute the position values.
-    new_level_x = new_left_pos // 256
-    new_pos_x = new_left_pos - (new_level_x * 256)
-    new_screen_offset_x = x
-
-    # Set the new positions.
-    ram[0x006D] = new_level_x
-    ram[0x0086] = new_pos_x
-    ram[0x03AD] = new_screen_offset_x
-
-    # Screen Y position.
-    ram[0x00CE] = y
-
-    # Horizontal speed.
-    #   0xD8 < 0 - Moving left
-    #   0x00 - Not moving
-    #   0 < 0x28 - Moving right
-    ram[0x0057] = 0
-
-    # Vertical velocity, whole pixels.
-    #   Upward: FB = normal jump
-    #   Downward: 05 = fastest fall
-    ram[0x009F] = 0
-
-    # Vertical velocity, fractional.
-    ram[0x0433] = 0
-
-
-def _get_value_at_offset(ram: NdArrayUint8, envs: Any, device: str, agent: Any, x: int, y: int, xvel: int, yvel: int) -> tuple[float, Any]:
-    # Put Mario into position and render.
-    #   1. Set RAM.
-    #   2. Step.
-    #   3. Render.
-    #   4. Put value function through.
-    #   5. Restore RAM.
-
-    ram_start = ram.copy()
-
-    # Set Mario's position and velocity.
-    _set_mario_pos_in_ram(ram, x=x, y=y, xvel=xvel, yvel=yvel)
-
-    # Use no-op action.
-    action_index = 0
-    action_np = np.array([action_index], dtype=np.int64)
-
-    # Step environment.
-    #   next_obs.shape: torch.Size([1, 4, 84, 84])
-    #   reward.shape: (1,)
-    next_obs, reward, terminations, truncations, infos = envs.step(action_np)
-
-    # Convert to pytorch Tensor
-    next_obs = torch.Tensor(next_obs).to(device)
-
-    # Retrieve value from reward.
-    #   value.shape: torch.Size([1, 1])
-    _action, _logprob, _, value = agent.get_action_and_value(next_obs)
-
-    # Restore ram.
-    ram[:] = ram_start
-
-    value_single = value[0][0]
-
-    return value_single, next_obs
-
-
-def _render_mario_pos_value_sweep(envs: Any, device: str, agent: Any):
-    w = 240
-    h = 224
-    env = envs.envs[0].unwrapped
-    ram = env.ai_handler.ram
-
-    # Sweep mario positions, get value function.
-    x_steps = list(range(0, w, 15))
-    y_steps = list(range(0, h, 15))
-    num_x = len(x_steps)
-    num_y = len(y_steps)
-    values_grid = np.zeros((num_y, num_x), dtype=np.float64)
-
-    for j, y in enumerate(y_steps):
-        for i, x in enumerate(x_steps):
-            value, obs = _get_value_at_offset(ram=ram, envs=envs, device=device, agent=agent, x=x, y=y, xvel=0, yvel=0)
-            values_grid[j][i] = value
-
-    # Normalize values to 0-255.
-    v_min = values_grid.min()
-    v_max = values_grid.max()
-    values_normalized = (values_grid - v_min) / (v_max - v_min)
-
-    # Convert values to grayscale image.
-    if _TEST_RANDOM_IMAGE := False:
-        #values_image_np_uint8 = np.random.randint(0, 256, size=(h, w), dtype=np.uint8)
-        values_image_np_uint8 = np.random.randint(200, 256, size=(h, w), dtype=np.uint8)
-    else:
-        values_image_np_uint8 = (values_normalized * 255).astype(np.uint8)
-
-    values_gray = Image.fromarray(values_image_np_uint8, mode='L').resize((w, h), resample=Image.Resampling.NEAREST)
-
-    # Convert to RGB.
-    values_rgba = values_gray.convert('RGBA')
-    assert values_rgba.size == (w, h), f"Unexpected values_rgba.size: {values_rgba.size} != {(w,h)}"
-
-    # Create an image of the regular screen, with high alpha.
-    if False:
-        obs_np = obs[-1][-1].to(torch.uint8).numpy()
-        obs_grayscale = Image.fromarray(obs_np, mode='L').resize((w, h), resample=Image.Resampling.NEAREST)
-        obs_rgba = obs_grayscale.convert('RGBA')
-        obs_rgba.putalpha(int(0.25 * 256))
-    else:
-        obs_image = env.screen.get_image(screen_index=0)
-        obs_rgba = obs_image.convert('RGBA')
-        obs_rgba.putalpha(int(0.15 * 256))
-
-    # Paste the current observation onto the image.  Take the mask from the alpha values.
-    values_rgba.paste(obs_rgba, (0, 0), mask=obs_rgba)
-
-    # Set screen values.
-    values_rgb = values_rgba.convert('RGB')
-    env.screen.set_image(values_rgb, screen_index=1)
 
 
 def main():
@@ -481,30 +317,9 @@ def main():
             next_done = np.logical_or(terminations, truncations)
             rewards[step] = torch.tensor(reward).to(device).view(-1)
 
-            orig_obs = next_obs
-
             # NOTE: Silent conversion to float32 for Tensor.
             next_obs = torch.Tensor(next_obs).to(device)
             next_done = torch.Tensor(next_done).to(device)
-
-            if False:
-                env = envs.envs[0].unwrapped
-
-                # NOTE: There was a silent conversion from uint8 observations to float.  Change it back to uint8 for display.
-                assert orig_obs.dtype == np.uint8
-                obs_cpu = next_obs.to(torch.uint8).numpy()
-
-                if False:
-                    assert (orig_obs == obs_cpu).all(), f"mismatch cpu:\n{orig_obs=}\n{obs_cpu}"
-                    assert (orig_obs.shape == obs_cpu.shape), f"mismatch shape:\n{orig_obs.shape=}\n{obs_cpu.shape=}"
-                    assert (orig_obs.dtype == obs_cpu.dtype), f"mismatch dtype:\n{orig_obs.dtype=}\n{obs_cpu.dtype=}"
-
-                obs_single = obs_cpu[-1][-1]
-
-                second_screen_image = Image.fromarray(obs_single, 'L').resize((240, 224), Image.Resampling.NEAREST).convert('RGB')
-
-                # env.second_screen_image=<PIL.Image.Image image mode=RGB size=240x224 at 0x16BF48710>
-                env.second_screen_image = second_screen_image
 
             if "final_info" in infos:
                 for info in infos["final_info"]:
@@ -515,7 +330,7 @@ def main():
 
         steps_end = time.time()
 
-        if False:
+        if True:
             optimize_networks_start = time.time()
 
             # bootstrap value if not done
@@ -648,8 +463,10 @@ def main():
                 print(f"Checkpoint done: {time.time() - start_checkpoint:.4f}s")
 
         # Show value sweep.
-        if False: #iteration % 5 == 0:
-            _render_mario_pos_value_sweep(envs=envs, device=device, agent=agent)
+        if iteration % 1 == 0:
+            env = envs.envs[0].unwrapped
+            values_sweep_rgb = render_mario_pos_value_sweep(envs=envs, device=device, agent=agent)
+            env.screen.set_image(values_sweep_rgb, screen_index=1)
 
     envs.close()
     writer.close()
---
+++ super_mario_env_viz.py
@@ -0,0 +1,175 @@
+from typing import Any, Literal, Optional
+
+import numpy as np
+import torch
+from PIL import Image
+
+NdArrayUint8 = np.ndarray[np.dtype[np.uint8]]
+NdArrayRGB8 = np.ndarray[tuple[Literal[4]], np.dtype[np.uint8]]
+
+
+def _set_mario_pos_in_ram(ram: NdArrayUint8, x: int, y: int, xvel: int, yvel: int):
+
+    # Current position.
+    # E.g.:
+    #   level_x=  1 pos_x=118 screen_offset= 25
+    #   level_x=  4 pos_x= 45 screen_offset=106
+    #   level_x=  3 pos_x=195 screen_offset=  0
+
+    # Player horizontal position in level.  Increments for each new full screen.
+    level_x = ram[0x006D]
+
+    # Player X position.
+    pos_x = ram[0x0086]
+
+    # Player x pos within current screen offset.
+    screen_offset_x = ram[0x03AD]
+
+    # When we set "x=0", we want the player position to be at the left edge of the screen.
+    # But, we may be transitioning between screen chunks.
+    #
+    # Example going back and forth between boundary of screen chunk:
+    #   Iter: 2726   Time left: 379   level_x=  1 screen_x=  9 screen_offset=112
+    #   Iter: 31238  Time left: 358   level_x=  1 screen_x= 33 screen_offset= 84
+    #   Iter: 41750  Time left: 337   level_x=  0 screen_x=205 screen_offset=  0
+    #   Iter: 52262  Time left: 315   level_x=  0 screen_x=205 screen_offset=  0
+    #   Iter: 62774  Time left: 294   level_x=  0 screen_x=205 screen_offset=  0
+    #   Iter: 73286  Time left: 273   level_x=  1 screen_x=  5 screen_offset= 56
+    #   Iter: 83798  Time left: 251   level_x=  1 screen_x=  5 screen_offset= 56
+    #   Iter: 94310  Time left: 230   level_x=  1 screen_x= 35 screen_offset= 80
+    #   Iter: 10822  Time left: 209   level_x=  1 screen_x=182 screen_offset=111
+    #   Iter: 11334  Time left: 187   level_x=  1 screen_x=249 screen_offset=112
+    #   Iter: 12846  Time left: 166   level_x=  2 screen_x=  2 screen_offset=112
+    #   Iter: 13358  Time left: 145   level_x=  1 screen_x=227 screen_offset= 66
+    #   Iter: 14870  Time left: 123   level_x=  1 screen_x=247 screen_offset= 80
+    #   Iter: 15382  Time left: 102   level_x=  2 screen_x= 11 screen_offset= 79
+    #   Iter: 16894  Time left: 81    level_x=  1 screen_x=249 screen_offset= 61
+    #   Iter: 17406  Time left: 59    level_x=  2 screen_x= 41 screen_offset= 83
+    #
+    # When the offset is greater than the screen position, we have to roll back the level.
+
+    # Convert into an absolute left position (instead of relative to screen chunk).
+    left_pos = int(level_x) * 256 + int(pos_x)
+
+    # The left side of the screen is going to be when screen_offset_x is 0.
+    new_left_pos = left_pos - int(screen_offset_x) + x
+
+    # Recompute the position values.
+    new_level_x = new_left_pos // 256
+    new_pos_x = new_left_pos - (new_level_x * 256)
+    new_screen_offset_x = x
+
+    # Set the new positions.
+    ram[0x006D] = new_level_x
+    ram[0x0086] = new_pos_x
+    ram[0x03AD] = new_screen_offset_x
+
+    # Screen Y position.
+    ram[0x00CE] = y
+
+    # Horizontal speed.
+    #   0xD8 < 0 - Moving left
+    #   0x00 - Not moving
+    #   0 < 0x28 - Moving right
+    ram[0x0057] = 0
+
+    # Vertical velocity, whole pixels.
+    #   Upward: FB = normal jump
+    #   Downward: 05 = fastest fall
+    ram[0x009F] = 0
+
+    # Vertical velocity, fractional.
+    ram[0x0433] = 0
+
+
+def _get_value_at_offset(ram: NdArrayUint8, envs: Any, device: str, agent: Any, x: int, y: int, xvel: int, yvel: int) -> tuple[float, Any]:
+    # Put Mario into position and render.
+    #   1. Set RAM.
+    #   2. Step.
+    #   3. Render.
+    #   4. Put value function through.
+    #   5. Restore RAM.
+
+    ram_start = ram.copy()
+
+    # Set Mario's position and velocity.
+    _set_mario_pos_in_ram(ram, x=x, y=y, xvel=xvel, yvel=yvel)
+
+    # Use no-op action.
+    action_index = 0
+    action_np = np.array([action_index], dtype=np.int64)
+
+    # Step environment.
+    #   next_obs.shape: torch.Size([1, 4, 84, 84])
+    #   reward.shape: (1,)
+    next_obs, reward, terminations, truncations, infos = envs.step(action_np)
+
+    # Convert to pytorch Tensor
+    next_obs = torch.Tensor(next_obs).to(device)
+
+    # Retrieve value from reward.
+    #   value.shape: torch.Size([1, 1])
+    _action, _logprob, _, value = agent.get_action_and_value(next_obs)
+
+    # Restore ram.
+    ram[:] = ram_start
+
+    value_single = value[0][0]
+
+    return value_single, next_obs
+
+
+def render_mario_pos_value_sweep(envs: Any, device: str, agent: Any):
+    w = 240
+    h = 224
+    env = envs.envs[0].unwrapped
+    ram = env.ai_handler.ram
+
+    # Sweep mario positions, get value function.
+    x_steps = list(range(0, w, 15))
+    y_steps = list(range(0, h, 15))
+    num_x = len(x_steps)
+    num_y = len(y_steps)
+    values_grid = np.zeros((num_y, num_x), dtype=np.float64)
+
+    for j, y in enumerate(y_steps):
+        for i, x in enumerate(x_steps):
+            value, obs = _get_value_at_offset(ram=ram, envs=envs, device=device, agent=agent, x=x, y=y, xvel=0, yvel=0)
+            values_grid[j][i] = value
+
+    # Normalize values to 0-255.
+    v_min = values_grid.min()
+    v_max = values_grid.max()
+    values_normalized = (values_grid - v_min) / (v_max - v_min)
+
+    # Convert values to grayscale image.
+    if _TEST_RANDOM_IMAGE := False:
+        #values_image_np_uint8 = np.random.randint(0, 256, size=(h, w), dtype=np.uint8)
+        values_image_np_uint8 = np.random.randint(200, 256, size=(h, w), dtype=np.uint8)
+    else:
+        values_image_np_uint8 = (values_normalized * 255).astype(np.uint8)
+
+    values_gray = Image.fromarray(values_image_np_uint8, mode='L').resize((w, h), resample=Image.Resampling.NEAREST)
+
+    # Convert to RGB.
+    values_rgba = values_gray.convert('RGBA')
+    assert values_rgba.size == (w, h), f"Unexpected values_rgba.size: {values_rgba.size} != {(w,h)}"
+
+    # Create an image of the regular screen, with high alpha.
+    if False:
+        obs_np = obs[-1][-1].to(torch.uint8).numpy()
+        obs_grayscale = Image.fromarray(obs_np, mode='L').resize((w, h), resample=Image.Resampling.NEAREST)
+        obs_rgba = obs_grayscale.convert('RGBA')
+        obs_rgba.putalpha(int(0.25 * 256))
+    else:
+        obs_image = env.screen.get_image(screen_index=0)
+        obs_rgba = obs_image.convert('RGBA')
+        obs_rgba.putalpha(int(0.15 * 256))
+
+    # Paste the current observation onto the image.  Take the mask from the alpha values.
+    values_rgba.paste(obs_rgba, (0, 0), mask=obs_rgba)
+
+    # Set screen values.
+    values_rgb = values_rgba.convert('RGB')
+
+    return values_rgb
